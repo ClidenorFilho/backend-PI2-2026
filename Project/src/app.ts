c2694bf8b4c5d8c37f2e09ade4f85097df5095f2
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-04-23 00:11:51 -0300

    feat: integração da tela de login e cadastro com a API

--- Project/src/app.ts
+++ Project/src/app.ts
@@ -7,10 +7,12 @@
 import express, { Request, Response, NextFunction } from "express";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
+import cors from 'cors';
 
 const app = express();
 
 // ── Middlewares globais ───────────────────────────────────────────
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
